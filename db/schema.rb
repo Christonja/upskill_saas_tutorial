e890ee8463121723e27911b94ac3d01701cff798
Christon Altmann <christonja@gmail.com>
2018-06-28 03:12:17 +0000

    Added create action to save to db and fixed contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628020936) do
+ActiveRecord::Schema.define(version: 20180628030227) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
